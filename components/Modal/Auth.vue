13c7d78e8428714c1ac468338e62c3a791b8b064
realdavone <davidkuljovsky@gmail.com>
2024-08-19 18:24:59 +0200

    adjusted login modalis

--- components/Modal/Auth.vue
+++ components/Modal/Auth.vue
@@ -1,35 +1,43 @@
 <template>
 	<dialog ref="dialogRef">
-		<header>
+		<button class="close-button" @click="close">&times;</button>
+
+		<div class="image-container">
+			<img src="/images/login.jpeg" alt="logo" />
+		</div>
+
+		<div class="content">
 			<h2>Prihlásiť sa</h2>
 
-			<button @click="close">&times;</button>
-		</header>
-
-		<form @submit.prevent="signIn">
-			<label for="username">Používateľské meno</label>
-			<input
-				id="username"
-				name="username"
-				required
-				v-model="credentials.username"
-			/>
-
-			<label for="password">Heslo</label>
-			<input
-				type="password"
-				id="password"
-				name="password"
-				required
-				v-model="credentials.password"
-			/>
-
-			<p v-if="error" class="error">
-				{{ error?.message }}
+			<p class="description">
+				Sledujte vaše obľúbené filmy a sériály. Stačí sa prihlásiť a začať sledovať.
 			</p>
 
-			<Button type="submit">Prihlásiť sa</Button>
-		</form>
+			<form @submit.prevent="signIn">
+				<label for="username">Používateľské meno</label>
+				<input
+					id="username"
+					name="username"
+					required
+					v-model="credentials.username"
+				/>
+
+				<label for="password">Heslo</label>
+				<input
+					type="password"
+					id="password"
+					name="password"
+					required
+					v-model="credentials.password"
+				/>
+
+				<p v-if="error" class="error">
+					{{ error?.message }}
+				</p>
+
+				<Button type="submit">Prihlásiť sa</Button>
+			</form>
+		</div>
 	</dialog>
 </template>
 
@@ -76,30 +84,56 @@ dialog {
 	background-color: rgb(30, 30, 30);
 	max-width: min(450px, calc(100% - 2rem));
 	width: 100%;
-	border-radius: 0.75rem;
-	padding: 1.5rem;
+	border-radius: 1rem;
 
-	header {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		gap: 1rem;
-		margin-bottom: 1rem;
-
-		h2 {
-			font-size: 1.5rem;
-			font-weight: 700;
+	.image-container {
+		img {
+			opacity: 0.5;
+			width: 100%;
+			height: auto;
+			object-fit: cover;
 		}
+	}
 
-		button {
-			all: unset;
-			cursor: pointer;
-			font-size: 2rem;
-			font-weight: 500;
-			color: #fff;
-			align-self: flex-start;
-			line-height: 1;
-		}
+	.content {
+		margin-top: 1rem;
+		padding-inline: 1.5rem;
+		padding-bottom: 1.5rem;
+	}
+
+	h2 {
+		font-size: 1.5rem;
+		font-weight: 600;
+		text-align: center;
+		margin-bottom: 0.5rem;
+	}
+
+	.description {
+		text-align: center;
+		line-height: 1.4;
+		margin-bottom: 2rem;
+		opacity: 0.75;
+		font-size: smaller;
+	}
+
+	.close-button {
+		all: unset;
+		cursor: pointer;
+		font-size: 1.5rem;
+		font-weight: 500;
+		width: 1.5rem;
+		height: 1.5rem;
+		border-radius: 50%;
+		color: black;
+		line-height: 1;
+		position: absolute;
+		top: 1rem;
+		right: 1rem;
+		background-color: white;
+		display: grid;
+		place-items: center;
+		line-height: 1;
+		z-index: 1;
 	}
 
 	&::backdrop {
